f707edfba9cfcf66df14b47a1215da3da4e9f9c0
AIE\S214712 <S214712@students.aie.edu.au>
2023-09-06 16:13:30 +0930

    Added animation to Sammy, interaction with the different blocks,

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,13 +19,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float m_slingLimit;
+    [SerializeField]
+    private float m_slingPower;
     private float m_slingLength;
-
-    public BoxCollider2D m_boxCollider;
-
-    public string m_collisionTag;
+    private string m_collisionTag;
     
-        
+    private Animator m_animation;
+    SphereCollider m_collider;
+    
+    
+
+    
+
+
+           
     enum State
     {
         Stuck,
@@ -35,14 +44,15 @@ public class PlayerController : MonoBehaviour
     private State m_state = State.Stuck;
 
 
-    private Rigidbody2D m_playerBody;
+    private Rigidbody m_playerBody;
 
     private void Start()
     {
         m_sling = transform.GetChild(0).gameObject;
-        m_playerBody = GetComponent<Rigidbody2D>();
+        m_playerBody = GetComponent<Rigidbody>();
         m_worldSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-        m_boxCollider = GetComponent<BoxCollider2D>();
+        m_collider = GetComponent<SphereCollider>();
+        m_animation = GetComponent<Animator>();
     }
 
     private void Update()
@@ -81,21 +91,17 @@ public class PlayerController : MonoBehaviour
         {
             m_state = State.Dying;
         }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("State = " + m_state);
+        }
     }
-    
-
-    
-
-    public void Attach(GameObject box)
-    {
-        transform.SetParent(box.transform, false);
-
-    }
-
     
     public void Stuck()
     {
-        if(Input.GetMouseButton(0))
+        m_animation.Play("StickDown");
+        if(Input.GetMouseButtonDown(0))
         {
             m_state = State.Stretch;
         }
@@ -119,40 +125,111 @@ public class PlayerController : MonoBehaviour
         m_slingVector = m_slingLength * m_slingDirection;
         m_sling.transform.position = m_playerPos + m_slingVector;
         //On mouse up, remove from parent, add force and change state and turn off sling indicator
-        //if thre are any inconsistencies with physics, call physics in Fixed Update
-        if(Input.GetMouseButtonUp(0) && m_slingPos.y <= m_playerPos.y)
+        //if there are any inconsistencies with physics, call physics in Fixed Update
+        if(Input.GetMouseButtonUp(0))
         {
-            m_playerBody.AddForce(m_slingVector * -1, ForceMode2D.Impulse);
+            m_playerBody.AddForce(m_slingVector * -m_slingPower, ForceMode.Impulse);
             m_sling.SetActive(false);
+                      
             m_state = State.Flying;
         }
-        if (Input.GetMouseButtonUp(0) && m_slingPos.y > m_playerPos.y)
-        {
-            m_playerBody.AddForce(m_slingVector * -1, ForceMode2D.Impulse);
-            m_sling.SetActive(false);
-            m_state = State.Falling;
-        }
+        
     }
 
     public void Flying()
     {
-        
+        m_animation.Play("FlyAnimation");
+        transform.up = m_playerBody.velocity.normalized;
+        transform.localScale = Vector3.one * 0.8f;
     }
 
     public void Falling()
     {
-
+        m_animation.Play("FallAnimation");
     }
 
     public void Dying()
     {
-        transform.position = new Vector3(0, 0, 0);
-        m_playerBody.velocity = Vector2.zero;        
+        m_animation.Play("DeathAnimation");
+        m_collider.enabled = false;
+        m_playerBody.useGravity = false;
+        StartCoroutine(ResetPosition());
     }
 
-    
+    public void OnCollisionEnter(Collision other)
+    {
+        //check what type of block the player is colliding with
+        m_collisionTag = other.gameObject.tag;
+        
+        switch(m_collisionTag)
+        {
+            case "Block":
+                m_state = State.Stuck;
+                m_playerBody.useGravity = false;
+                m_playerBody.velocity = Vector3.zero;
+                transform.up = other.contacts[0].normal;
+                transform.SetParent(other.transform, true); 
+                break;
+           case "Ice":
+                m_state = State.Stuck;
+                transform.SetParent(other.transform, true);
+                transform.up = other.contacts[0].normal;
+                break;
+           case "Break":
+                m_state = State.Stuck;
+                m_playerBody.useGravity = false;
+                m_playerBody.velocity = Vector3.zero;
+                transform.up = other.contacts[0].normal;
+                transform.SetParent(other.transform, true);
+                StartCoroutine(DestroyBlock(other.gameObject));
+                break;
+           case "Death":
+                m_state = State.Dying;
+                break;
+        }
+    }
+   
+   
+   
+    public void OnCollisionExit(Collision other)
+    {
+        m_playerBody.useGravity = true;
+        transform.SetParent(null);
+    }
 
-    
-    
+
+    IEnumerator DestroyBlock(GameObject block)
+    {        
+        //wait half a second, and then destroy block
+        yield return new WaitForSecondsRealtime(0.5f);
+        //if player is still attached to block, detach
+        if(block == transform.parent.gameObject)
+        {
+            Detach();
+        }
+        Destroy(block);
+    }
+    IEnumerator ResetPosition()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        m_collider.enabled = true;
+        m_playerBody.useGravity = true;
+        transform.position = new Vector3(0, -2, 0);
+        m_playerBody.velocity = Vector2.zero;
+        m_state = State.Falling;
+    }
+
+    private void Detach()
+    {
+        //remove block as parent, and enter falling state
+        transform.SetParent(null);
+        m_playerBody.useGravity = true;
+        m_state = State.Falling;
+        if (m_sling.activeInHierarchy== true)
+        {
+            m_sling.SetActive(false);
+        }
+    }
+
 
 }
