00b4a8e886d7643cc8df8575ca8d7e6c907beeee
AIE\s213853 <s213853@students.aie.edu.au>
2023-09-07 11:34:43 +0930

    Spawn thing changeed so that it increases over time.

--- Assets/Scripts/Environment/BlockMovement.cs
+++ Assets/Scripts/Environment/BlockMovement.cs
@@ -12,13 +12,17 @@ public class BlockMovement : MonoBehaviour
 
     public float m_fallSpeed;
 
+    //public GameObject m_spawnBlocks;
+    private SpawnBlocks m_sb;
+
     //private SpawnBlocks m_sb;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody>();
-        //m_sb.GetComponent<SpawnBlocks>();
+        //m_spawnBlocks = GetComponentInParent<GameObject>();
+        m_sb = GetComponentInParent<SpawnBlocks>();
     }
 
     // Update is called once per frame
@@ -26,6 +30,7 @@ public class BlockMovement : MonoBehaviour
     {
         //m_rb.velocity = Vector3.down * m_fallSpeed;
         transform.Translate(Vector3.down * m_fallSpeed * Time.deltaTime);
+        m_fallSpeed = m_sb.blockFallSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,4 +45,6 @@ public class BlockMovement : MonoBehaviour
             Destroy(gameObject);
         }   
     }
+
+    
 }
--- Assets/Scripts/Environment/SpawnBlocks.cs
+++ Assets/Scripts/Environment/SpawnBlocks.cs
@@ -18,6 +18,13 @@ public class SpawnBlocks : MonoBehaviour
     //To count frames before adjusting speed (This should be edited later to account for height)
     private float m_speedCounter;
 
+    private bool m_startGame = true;
+
+    private bool m_changeSpeed = true;
+
+    private float m_shortestSpawnTime = 4.0f;
+    private float m_longestSpawnTime = 5.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,21 +34,49 @@ public class SpawnBlocks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spawn = SpawnBlock(spawnBlocks[Random.Range(0, 7)], Random.Range(4.0f, 7.5f));
+        spawn = SpawnBlock(spawnBlocks[Random.Range(0, 7)], Random.Range(m_shortestSpawnTime, m_longestSpawnTime));
         if (m_canSpawn)
         {
             StartCoroutine(spawn);
         }
 
+        if (m_startGame)
+        {
+            StartGame();
+        }
+
+        if (m_changeSpeed)
+        {
+            StartCoroutine(DifficultyChange());
+        }
     }
 
     IEnumerator SpawnBlock(GameObject block, float time)
     {
         m_canSpawn = false;
         yield return new WaitForSeconds(time);
-        Instantiate(block, transform.position, Quaternion.identity);
+        Instantiate(block, transform.position, Quaternion.identity, transform);
         m_canSpawn = true;
         
+    }
+
+    IEnumerator DifficultyChange()
+    {
+        m_changeSpeed = false;
+        Debug.Log("Script called");
+        yield return new WaitForSeconds(20f);
+        Debug.Log("Speed has changed");
+        blockFallSpeed += 0.1f;
+        m_shortestSpawnTime -= 0.1f;
+        m_longestSpawnTime -= 0.1f;
+        m_changeSpeed = true;
+    }
+
+    public void StartGame()
+    {
+        blockFallSpeed = 0.5f;
+        m_startGame = false;
+        m_changeSpeed = true;
     }
 
 }
