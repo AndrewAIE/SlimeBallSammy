93c9a6c20bbb0d7eefb9e8b1f312471212729aeb
AIE\s213846 <s213846@students.aie.edu.au>
2023-09-20 10:50:30 +0930

    randomised death screen text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using System.Security.Cryptography;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Samuel;
     public GameObject timer;
     public GameObject DeathScreen;
+    public TMP_Text DeathText;
+
+    private int randomNumber;
+
 
     public ButtonEvents ButtonEvents;
 
@@ -28,11 +36,38 @@ public class GameManager : MonoBehaviour
             EndGame();
         }
     }
+    public void Start()
+    {
+        // get ranbdom number for deat text
+        randomNumber = Random.Range(0, 100);
+    }
 
     private void EndGame()
-    {        
+    { 
+        
         DeathScreen.SetActive(true);
         Time.timeScale = 0f;
+        
+        // set text depending on the random number value
+        if (randomNumber >= 70)
+            DeathText.text = "Well this is unfortunate.";
+
+        if (randomNumber < 70 &  randomNumber >= 60)
+            DeathText.text = "We all jump, we all fall.";
+
+        if (randomNumber < 60 & randomNumber >= 40)
+            DeathText.text = "Slimeball go splat!";
+
+        if (randomNumber < 40 & randomNumber >= 30)
+            DeathText.text = "Did that hurt?";
+
+        if (randomNumber < 30 & randomNumber >= 2)
+            DeathText.text = "Death is an inevitable part of life.";
+
+        if (randomNumber <= 1)
+            DeathText.text = "you died, but im never gonna give you up...";
+
+
     }
 
     public bool ClickerActive()
